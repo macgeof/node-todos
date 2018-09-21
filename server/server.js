1c43cdcd42573d793f521cb31c1cd0713641fe86
Geof Brownbridge <geof@Geofs-MacBook-Pro.local>
2018-09-21 16:07:24 +0100

    Make config generic for different environments - test, dev and prod

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -8,7 +10,6 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -135,8 +136,8 @@ app.patch('/todos/:id', (request, response) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Started on port ${process.env.PORT}`);
 });
 
 module.exports = {
