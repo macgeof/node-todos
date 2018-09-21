9abf5785cb5e5e2a6db73bbf87bd6114b373f6ed
Geof Brownbridge <geof@Geofs-MacBook-Pro.local>
2018-09-21 12:56:12 +0100

    Add test cases for GET /todos/:id endpoint

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,20 +1,20 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-const todos = [
-  {
-    text:'First test todo'
-  },
-  {
-    text: 'Second test todo'
-  }
-];
+const todos = [{
+  _id: new ObjectID(),
+  text: 'First test todo'
+}, {
+  _id: new ObjectID(),
+  text: 'Second test todo'
+}];
 
 beforeEach((done) => {
-  Todo.remove({})
+  Todo.deleteMany()
     .then(() => {
       return Todo.insertMany(todos);
     })
@@ -79,6 +79,33 @@ describe('GET /todos endpoint', () => {
       .expect((response) => {
         expect(response.body.todos.length).toBe(2)
       })
+      .end(done);
+  })
+});
+
+describe('GET /todos:id endpoint', () => {
+  it('should get a single todo by id', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((response) => {
+        expect(response.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return a 404 if todo not found', (done) => {
+    const id = new ObjectID().toHexString();
+    request(app)
+      .get(`/todos/${id}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return a 404 for non valid object id', (done) => {
+    request(app)
+      .get('/todos/123')
+      .expect(404)
       .end(done);
   })
 });
