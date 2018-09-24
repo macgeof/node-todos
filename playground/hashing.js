6d9f2f947adc63228bb4c64b0173c3c48584df9e
Geof Brownbridge <geof@geofs-mbp.home>
2018-09-24 15:58:44 +0100

    Hash user password before saving to mongodb

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,27 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const data = {
-  id : 10
-};
-const token = jwt.sign(data, '123abc');
-console.log(token);
-const decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+const password = '123abc!';
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+const hashedPassword = '$2a$10$JQcb1NwWxppPF4nfncyIkeM/XaM6w6nsnrhHRPUsjHbgpjSaLGCPG';
+
+bcrypt.compare(password, hashedPassword, (err, response) => {
+  console.log(response);
+})
+
+// const data = {
+//   id : 10
+// };
+// const token = jwt.sign(data, '123abc');
+// console.log(token);
+// const decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 // const message = 'I am user number 3';
 //
